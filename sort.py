d7712bd5307b26e6a0d7c8df653a316d10c461ea
logotip <logotip123@yahoo.com>
2019-11-25 11:52:28 +0200

    v 1.0

--- sort.py
+++ sort.py
@@ -1,5 +1,19 @@
+"""list sort module"""
 from typing import List
 
 
-def sort(l: List[int]):
-    ...
+def sort(nums: List[int]):
+    """
+    sorting by inserts algorithm function
+    :param nums: list for sorting
+    :return: sorted list
+    """
+    for i in range(1, len(nums)):
+        item_to_insert = nums[i]
+        past_i = i - 1
+        while past_i >= 0 and nums[past_i] > item_to_insert:
+            nums[past_i + 1] = nums[past_i]
+            past_i -= 1
+        # Вставляем элемент
+        nums[past_i + 1] = item_to_insert
+    return nums

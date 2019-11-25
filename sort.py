8f1c3b0147e1c9fbca2a81be18009033fa36081e
Dima <dima.sapsay@gmail.com>
2019-11-25 16:37:17 +0200

    first commit

--- sort.py
+++ sort.py
@@ -1,5 +1,17 @@
+"""
+Sort a list in-place without using built-in functions (sort(), sorted(), etc)
+"""
+
 from typing import List
 
 
-def sort(l: List[int]):
-    ...
+def sort(lst: List[int]):
+    """Sort func"""
+    len_lst = len(lst)
+    for i in range(len_lst):
+        lowest_index = i
+        len_lst = len(lst)
+        for j in range(i+1, len_lst):
+            if lst[j] < lst[lowest_index]:
+                lowest_index = j
+        lst[i], lst[lowest_index] = lst[lowest_index], lst[i]

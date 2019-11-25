33e5553cdb6af7cef71e273b9358d0505cc6e5f3
d1makov <makovskyj.danylo@gmail.com>
2019-11-25 11:05:58 +0200

    final

--- sort.py
+++ sort.py
@@ -1,5 +1,17 @@
+"""
+docstring
+"""
 from typing import List
 
 
-def sort(l: List[int]):
-    ...
+def sort(lst: List[int]):
+    """
+
+    :param lst:
+    :return:
+    """
+    for i in range(len(lst)):
+        for j in range(1, len(lst)-i):
+            if lst[j-1] > lst[j]:
+                lst[j-1], lst[j] = lst[j], lst[j-1]
+    return lst
--- test_sort.py
+++ test_sort.py
@@ -1,9 +1,16 @@
+"""
+docstring
+"""
 import pytest
 
 import sort
 
 
 def test_sort():
+    """
+
+    :return:
+    """
     l = [2, 1, 3, -5, 3, 4]
     sort.sort(l)
     assert l == [-5, 1, 2, 3, 3, 4]

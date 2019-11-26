6d91f57fcf20c14fe65cbe4ad7ffc53de7690b52
Platoniuks <platon@alterpost.org>
2019-11-26 07:55:55 +0200

    resolved

--- sort.py
+++ sort.py
@@ -1,5 +1,18 @@
+"""Classic Bubble sort"""
+
 from typing import List
 
 
-def sort(l: List[int]):
-    ...
+def sort(arr: List[int]):
+    """General function"""
+    index = 0
+    swap_count = 0
+    while index < len(arr):
+        if index + 1 != len(arr) and arr[index] > arr[index + 1]:
+            arr[index], arr[index+1] = arr[index+1], arr[index]
+            swap_count = 1
+        index += 1
+        if index == len(arr) and swap_count == 1:
+            swap_count = 0
+            index = 0
+    return arr

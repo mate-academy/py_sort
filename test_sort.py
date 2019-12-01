a7a2ad4edfc49b4897260865d379d7cc2989e4bd
koreyo <KoretsValentyn@gmail.com>
2019-12-01 20:04:38 +0200

    bubble sort

--- test_sort.py
+++ test_sort.py
@@ -4,6 +4,6 @@ import sort
 
 
 def test_sort():
-    l = [2, 1, 3, -5, 3, 4]
-    sort.sort(l)
-    assert l == [-5, 1, 2, 3, 3, 4]
+    list_for_sort = [2, 1, 3, -5, 3, 4]
+    sort.sort(list_for_sort)
+    assert list_for_sort == [-5, 1, 2, 3, 3, 4]

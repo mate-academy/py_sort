24b090ea633758b607606ffd9e603cbee57db936
zaikovlad95@gmail.com <Makepeacenotwar1995>
2019-12-09 00:10:53 +0200

    Sort function.

--- sort.py
+++ sort.py
@@ -1,5 +1,13 @@
+"""Sort module."""
 from typing import List
 
 
-def sort(l: List[int]):
-    ...
+def sort(input_list: List[int]):
+    """sort a list using bubble sort."""
+    for i in range((len(input_list)) - 1):
+        for j in range((len(input_list)) - i - 1):
+            if input_list[j] > input_list[j + 1]:
+                buff = input_list[j]
+                input_list[j] = input_list[j + 1]
+                input_list[j + 1] = buff
+    return input_list
